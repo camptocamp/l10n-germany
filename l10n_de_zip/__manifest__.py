b0e1fe62073cbb07479577ef6b2301fcabcb8d69
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-07-03 11:40:05 +0200

    [ADD] l10n_de_zip

---
+++ l10n_de_zip/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Germany - Postal codes (ZIP) list',
+    'summary': 'Provides all German postal codes for auto-completion',
+    'version': '11.1.0.0',
+    'category': 'Localisation/Europe',
+    'depends': [
+        'base',
+        'base_location',
+        'l10n_de_country_states'
+    ],
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        'data/l10n_de_zip.xml'
+    ],
+    'installable': True,
+}
